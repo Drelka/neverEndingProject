f25db8062a4fc62a682fef3ac6b961a9443ba5f4
Drelka <47503020+Drelka@users.noreply.github.com>
2026-02-07 13:31:18 +0000

    exercise 01 - done

---
+++ re-learning/foundations-data-types-and-conditionals/script.js
@@ -0,0 +1,6 @@
+let siblings = null; // null is it's own type, null, not a number nor a string
+// in js represents "nothing", "empty", "value unknown"
+
+let education = undefined; // also type of it's own, like null
+// the meaning of undefined is "value is not assigned"
+
---
+++ re-learning/foundations-variables-and-operators/javascript.js
@@ -0,0 +1,39 @@
+let firstName = "John";
+let lastName = "Doe";
+
+console.log(firstName);
+console.log(lastName);
+
+
+let age = 11; // -declaration-
+console.log(age); // outputs 11
+
+age = 54; // -re-assignment-
+
+console.log(age); // then outputs 54, but previous 11 still exists, because it was called at that time
+
+
+const pi = 3.14;
+// pi = 10;
+
+// console.log(pi); // error is thrown, because we can't re-assign a constans value (they can naver change)
+
+
+console.log( (3 + 2) - 76 * (1 + 1) );
+
+console.log(23 - 34);
+console.log(2 - 2 + 2 - 2 + 2 + 2);
+console.log(1 + 2 * 3 * 2 - 1);
+console.log(32 * (18 - 16));
+
+
+let a = 12
+console.log(a);
+
+let b = 5 * a;
+console.log(b);
+
+
+let max = 57;
+let actual = max - 13;
+let percentage = actual / max;
